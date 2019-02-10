69a001dba90801c83a8491c9438d3a492d6881d6
brymlee <brymlee1911@gmail.com>
2019-02-09 19:38:12 -0800

    Modified Beginning, Ending, FullTag. Added NestTag.

--- HtmlTag.hs
+++ HtmlTag.hs
@@ -1,16 +1,44 @@
+{-# LANGUAGE ExistentialQuantification #-}
 module HtmlTag where 
+
+class Tag_ a where
+    html :: a -> String
+
+data Tag = forall a. Tag_ a => Tag a 
+
+instance Tag_ Tag where
+    html (Tag a) = html a
 
 data Attribute = Attribute String String deriving (Eq, Show)
 
 data BeginningTag = BeginningTag String [Attribute] deriving (Eq, Show)
-               
-data EndingTag = EndingTag String
+beginningTag :: String -> [Attribute] -> Tag 
+beginningTag a b = Tag (BeginningTag a b)
+
+data EndingTag = EndingTag String deriving (Eq, Show)
+endingTag :: String -> Tag
+endingTag a = Tag (EndingTag a)
+
+data FullTag = FullTag BeginningTag (Maybe String) EndingTag deriving (Eq, Show)
+fullTag :: BeginningTag -> Maybe String -> EndingTag -> Tag
+fullTag a b c = Tag (FullTag a b c)
+
+data Nest = Nest String [Tag] 
 
 concatenateAttributesAsString :: [Attribute] -> String 
-concatenateAttributesAsString attributes = foldr (++) " " (map (\ (Attribute name value) -> name ++ "=" ++ value) attributes)
+concatenateAttributesAsString attributes = (init $ foldr (++) " " (map (\ (Attribute name value) -> name ++ "=" ++ value) attributes))
 
-class Html a where
-    html :: a -> String
+instance Tag_ BeginningTag where
+    html (BeginningTag name attributes) | length attributes == 0 = "<" ++ name ++ "" ++ (concatenateAttributesAsString attributes) ++ ">"
+                                        | length attributes > 0 = "<" ++ name ++ " " ++ (concatenateAttributesAsString attributes) ++ ">"
 
-instance Html BeginningTag where
-    html (BeginningTag name attributes) = "<" ++ name ++ " " ++ (concatenateAttributesAsString attributes) ++ ">"
+instance Tag_ EndingTag where
+    html (EndingTag name) = "</" ++ name ++ ">"
+
+instance Tag_ FullTag where
+    html (FullTag beginningTag (Just text) endingTag) = (html beginningTag) ++ text ++ (html endingTag) 
+
+instance Tag_ Nest where
+    html (Nest parentLevelTagName children) = (html (BeginningTag parentLevelTagName [])) 
+                                            ++ (foldr (++) "" (map (\ (Tag a) -> html a) children))
+                                            ++ (html (EndingTag parentLevelTagName))
--- HtmlTagTest.hs
+++ HtmlTagTest.hs
@@ -3,12 +3,20 @@ import HtmlTag
 
 main :: IO()
 main = do
-    assertEquals "Does attribute with name as id and value as hello equal itself?" 
-        (Attribute "id" "hello") (Attribute "id" "hello")
-    assertEquals "Does tag with name as div and one attribute with name as hello and value as goodbye equal itself?" 
-        (BeginningTag "div" [(Attribute "hello" "goodbye")])
-        (BeginningTag "div" [(Attribute "hello" "goodbye")])
-    assertEquals "When getting HTML for tag div then HTML should be correct." 
+    let attributeIdHello = (Attribute "id" "hello")
+    assertEquals "Does attribute with name as id and value as hello equal itself?" attributeIdHello attributeIdHello
+    let beginningTag = (BeginningTag "div" [(Attribute "hello" "goodbye")]) 
+    assertEquals "Does tag with name as div and one attribute with name as hello and value as goodbye equal itself?" beginningTag beginningTag
+    assertEquals "When getting HTML for beginning tag div then HTML should be correct." 
         (html (BeginningTag "div" [(Attribute "hello" "goodbye")]))
-        "<div hello=goodbye >"
+        "<div hello=goodbye>"
+    assertEquals "When getting HTML for ending tag div then HTML should be correct."
+        (html (EndingTag "div")) 
+        "</div>"
+    assertEquals "When getting HTML for full tag with p then HTML should be correct."
+        (html (FullTag (BeginningTag "p" []) (Just "hello") (EndingTag "p")))
+        "<p>hello</p>"
+    assertEquals "When getting HTML for basic hello world document then HTML should be correct."
+        (html (Nest "html" [(fullTag (BeginningTag "p" []) (Just "hello world") (EndingTag "p"))]))
+        "<html><p>hello world</p></html>"
         
